12c44784710be9e4006308cf3e1b63f1f9d31d90
jenzhng <zhongjenn@gmail.com>
2022-04-20 19:11:29 -0400

    Updates

---
+++ precache-manifest.2e9dc84b8f1655fe9e277550317c907b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1710a0d628819504a914ed351cf35252",
+    "url": "/react-dev-portfolio/index.html"
+  },
+  {
+    "revision": "3902522e59ad7f0490ab",
+    "url": "/react-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c064479c71d3a4b2a82c",
+    "url": "/react-dev-portfolio/static/css/main.6e23c995.chunk.css"
+  },
+  {
+    "revision": "3902522e59ad7f0490ab",
+    "url": "/react-dev-portfolio/static/js/2.2a0f61c2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-dev-portfolio/static/js/2.2a0f61c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c064479c71d3a4b2a82c",
+    "url": "/react-dev-portfolio/static/js/main.dcbc7ca8.chunk.js"
+  },
+  {
+    "revision": "89561de6494c1f21ea25",
+    "url": "/react-dev-portfolio/static/js/runtime-main.1f12b2ae.js"
+  }
+]);
