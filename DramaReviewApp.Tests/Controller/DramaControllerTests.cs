22766e30180e824c48d23f050ece3c28778455b7
jenzhng <zhongjenn@gmail.com>
2022-04-26 01:23:06 -0400

    Test files

---
+++ DramaReviewApp.Tests/Controller/DramaControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using DramaReviewApp.Controllers;
+using DramaReviewApp.Dto;
+using DramaReviewApp.Interfaces;
+using DramaReviewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DramaReviewApp.Tests.Controller
+{
+    public class DramaControllerTests
+    {
+        private readonly IDramaRepository _dramaRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+        public DramaControllerTests()
+        {
+            _dramaRepository = A.Fake<IDramaRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public void DramaController_GetDramas_ReturnOK()
+        {
+            //Arrange
+            var dramas = A.Fake<ICollection<DramaDto>>();
+            var dramaList = A.Fake<List<DramaDto>>();
+            A.CallTo(() => _mapper.Map<List<DramaDto>>(dramas)).Returns(dramaList);
+            var controller = new DramaController(_dramaRepository, _reviewRepository, _mapper);
+
+            //Act
+            var result = controller.GetDramas();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void DramaController_CreateDrama_ReturnOK()
+        {
+            //Arrange
+            int ownerId = 1;
+            int catId = 2;
+            var dramaMap = A.Fake<Drama>();
+            var drama = A.Fake<Drama>();
+            var dramaCreate = A.Fake<DramaDto>();
+            var dramas = A.Fake<ICollection<DramaDto>>();
+            var dramaList = A.Fake<IList<DramaDto>>();
+            A.CallTo(() => _dramaRepository.GetDramaTrimToUpper(dramaCreate)).Returns(drama);
+            A.CallTo(() => _mapper.Map<Drama>(dramaCreate)).Returns(drama);
+            A.CallTo(() => _dramaRepository.CreateDrama(ownerId, catId, dramaMap)).Returns(true);
+            var controller = new DramaController(_dramaRepository, _reviewRepository, _mapper);
+
+            //Act
+            var result = controller.CreateDrama(ownerId, catId, dramaCreate);
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ DramaReviewApp.Tests/Repository/DramaRepositoryTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using DramaReviewApp.Data;
+using DramaReviewApp.Models;
+using DramaReviewApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DramaReviewApp.Tests.Repository
+{
+    public class DramaRepositoryTests
+    {
+
+        private async Task<DataContext> GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Drama.CountAsync() <= 0)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    databaseContext.Drama.Add(
+                    new Drama()
+                    {
+                        Name = "Bob's Burgers",
+                        DramaCategories = new List<DramaCategory>()
+                            {
+                                new DramaCategory { Category = new Category() { Name = "Comedy"}}
+                            },
+                        Reviews = new List<Review>()
+                            {
+                                new Review { Title="Bob's Burgers",Text = "Best show ever", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Warner", LastName = "Smith" } },
+                                new Review { Title="Bob's Burgers", Text = "Love the Belchers", Rating = 4,
+                                Reviewer = new Reviewer(){ FirstName = "Taylor", LastName = "Lautner" } },
+                                new Review { Title="Bob's Burgers",Text = "Favorite Cartoon", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Jessica", LastName = "Langley" } },
+                            }
+                    });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
+        [Fact]
+        public async void DramaRepository_GetDrama_ReturnsDrama()
+        {
+            //Arrange
+            var name = "Bob's Burgers";
+            var dbContext = await GetDatabaseContext();
+            var dramaRepository = new DramaRepository(dbContext);
+
+            //Act
+            var result = dramaRepository.GetDrama(name);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Drama>();
+        }
+
+        [Fact]
+        public async void DramaRepository_GetDramaRating_ReturnDecimalBetweenOneAndTen()
+        {
+            //Arrange
+            var dramaId = 1;
+            var dbContext = await GetDatabaseContext();
+            var dramaRepository = new DramaRepository(dbContext);
+
+            //Act
+            var result = dramaRepository.GetDramaRating(dramaId);
+
+            //Assert
+            result.Should().NotBe(0);
+            result.Should().BeInRange(1, 10);
+        }
+    }
+}
